67883a5ede768cb424d176a8b21433721d79e5da
Henok <henokmicrometre@gmail.com>
2024-01-29 22:49:03 +0000

    added video

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import StreamingResponse
+
 app = FastAPI()
+some_file_path = "alprVideo.mp4"
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
@@ -13,6 +16,17 @@ def root():
     return {"message": "Hello World"}
 
 
-@app.get("/video", response_class=HTMLResponse)
+
+
+@app.get("/video")
+def main():
+    def iterfile():  # 
+        with open(some_file_path, mode="rb") as file_like:  # 
+            yield from file_like  # 
+
+    return StreamingResponse(iterfile(), media_type="video/mp4")    
+
+
+@app.get("/alpr", response_class=HTMLResponse)
 async def home_page(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})    
