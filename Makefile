262cfd52a14bf54fcfc159cd915e562e91f59011
Henok <henokmicrometre@gmail.com>
2024-05-28 11:44:17 +0100

    adding upload

--- Makefile
+++ Makefile
@@ -1,3 +1,19 @@
 .PHONY: run
-start:
+fast:
 	uvicorn main:app --host 0.0.0.0 --port 5000 --reload
+
+clean:
+	rm static/images/*.jpg
+
+
+update:
+	docker compose down 
+	docker compose pull
+	docker compose up -d --build
+
+restart: 
+	docker compose restart
+
+remove:
+	docker compose down -v
+	docker compose rm -f
--- main.py
+++ main.py
@@ -14,6 +14,11 @@ import asyncio
 import uvicorn
 from sse_starlette.sse import EventSourceResponse
 from fastapi import FastAPI, Request
+from typing import Annotated
+
+from fastapi import  File, UploadFile
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 
 from models import ItemPayload
@@ -24,6 +29,7 @@ some_file_path = "static/upload/alprVideo.mp4"
 app = FastAPI(debug=True) # type: ignore
 redis_client = redis.StrictRedis(host="0.0.0.0", port=6379, db=0, decode_responses=True)
 app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 app.add_middleware(
     CORSMiddleware,
@@ -38,25 +44,31 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.post("/alprd")
+@app.post("/alprd1/alprd1")
 async def get_alprd(request: Request):
+    req_header = request.headers
     client_host = request.client.host
     alpr_data = await request.json()  
     alpr_uuid = alpr_data["uuid"]
+    alpr_img = alpr_data["uuid"]
     alpr_plate_results = alpr_data["results"]
     alpr_plate = alpr_plate_results[0]["plate"]
+    alpr_img_url = 'http://'+ client_host + ':5000' + '/images/' + alpr_uuid + '.jpg'
     alpr_id = redis_client.incr("alpr_ids")
     redis_client.hset(
-            f"alpr_id:{alpr_id}",
-            mapping={
-                "alpr_id": alpr_id,
-                "alpr_uuid": alpr_uuid,
-                "alpr_plate": alpr_plate,
-            },
-        )
-    redis_client.hset("alpr_plate_to_id", alpr_plate, alpr_id)
+        f"alpr_plate:{alpr_plate}",
+        mapping={
+            "alpr_plate_id": alpr_plate,
+            "alpr_plate": alpr_plate,
+            "alpr_plate_img": alpr_img,
+            "alpr_plate_url": alpr_img_url,
+        },
+    ) 
+    redis_client.hset("alpr_plate_to_id", alpr_plate, alpr_plate)
+    redis_client.hset("alpr_plate_to_img_url", alpr_plate, alpr_img_url)
+    redis_client.hset("alpr_plate_to_img", alpr_plate, alpr_img)
     redis_client.publish("bigboxcode", alpr_plate)
-    print((client_host))
+    print((alpr_img_url))
     return {"client_host": client_host}
 
 
@@ -74,3 +86,29 @@ def main():
         with open(some_file_path, mode="rb") as file_like:  # 
             yield from file_like  # 
     return StreamingResponse(iterfile(), media_type="video/mp4")    
+
+
+
+@app.post("/files/")
+async def create_files(files: Annotated[list[bytes], File()]):
+    return {"file_sizes": [len(file) for file in files]}
+
+
+
+@app.get("/upload")
+async def main():
+    content = """
+<body>
+<form action="/files/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+
+</body>
+    """
+    return HTMLResponse(content=content)    
+
+
+@app.get('/')
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})    
